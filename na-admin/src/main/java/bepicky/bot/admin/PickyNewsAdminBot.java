d4567d99537da86f542947e410e6a46c916ad74a
v.yemelianov <v.yemelianov@voverc.com>
2020-08-03 21:36:54 +0700

    updated bot with config-server

--- na-admin/src/main/java/bepicky/bot/admin/PickyNewsAdminBot.java
+++ na-admin/src/main/java/bepicky/bot/admin/PickyNewsAdminBot.java
@@ -3,10 +3,7 @@ package bepicky.bot.admin;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Slf4j
 public class PickyNewsAdminBot extends TelegramLongPollingBot {
@@ -19,29 +16,29 @@ public class PickyNewsAdminBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        if (update.hasMessage()) {
-            try {
-                execute(handlerManager.manage(update.getMessage()));
-            } catch (TelegramApiException e) {
-                log.error(e.getMessage(), e);
-                //TODO: send to admin
-                SendMessage m = new SendMessage()
-                    .setChatId(update.getMessage().getChatId())
-                    .setText(e.getMessage());
-                try {
-                    execute(m);
-                } catch (TelegramApiException ex) {
-                    log.error(ex.getMessage(), ex);
-                }
-            }
-        } else if (update.hasCallbackQuery()) {
-            try {
-                CallbackQuery callbackQuery = update.getCallbackQuery();
-                execute(handlerManager.manageCallback(callbackQuery.getMessage(), callbackQuery.getData()));
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
-        }
+//        if (update.hasMessage()) {
+//            try {
+//                execute(handlerManager.manage(update.getMessage()));
+//            } catch (TelegramApiException e) {
+//                log.error(e.getMessage(), e);
+//                //TODO: send to admin
+//                SendMessage m = new SendMessage()
+//                    .setChatId(update.getMessage().getChatId())
+//                    .setText(e.getMessage());
+//                try {
+//                    execute(m);
+//                } catch (TelegramApiException ex) {
+//                    log.error(ex.getMessage(), ex);
+//                }
+//            }
+//        } else if (update.hasCallbackQuery()) {
+//            try {
+//                CallbackQuery callbackQuery = update.getCallbackQuery();
+//                execute(handlerManager.manageCallback(callbackQuery.getMessage(), callbackQuery.getData()));
+//            } catch (TelegramApiException e) {
+//                e.printStackTrace();
+//            }
+//        }
     }
 
     @Override
