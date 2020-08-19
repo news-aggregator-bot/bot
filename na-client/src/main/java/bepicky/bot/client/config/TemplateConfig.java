d3b5f7f0192c07c9818dc1b6916d693a4801ae7e
v.yemelianov <v.yemelianov@voverc.com>
2020-08-19 12:26:33 +0700

    added repository to pom

--- na-client/src/main/java/bepicky/bot/client/config/TemplateConfig.java
+++ na-client/src/main/java/bepicky/bot/client/config/TemplateConfig.java
@@ -1,24 +1,18 @@
 package bepicky.bot.client.config;
 
-import com.google.common.io.Resources;
-import freemarker.cache.FileTemplateLoader;
 import freemarker.template.TemplateExceptionHandler;
 import freemarker.template.Version;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URISyntaxException;
-
 @Configuration
 public class TemplateConfig {
 
     @Bean("localTemplateConfig")
-    public freemarker.template.Configuration templateConfiguration() throws IOException, URISyntaxException {
+    public freemarker.template.Configuration templateConfiguration() {
 
         freemarker.template.Configuration cfg = new freemarker.template.Configuration(new Version(2, 3, 20));
-        cfg.setTemplateLoader(new FileTemplateLoader(new File(Resources.getResource("templates").toURI())));
+        cfg.setClassForTemplateLoading(this.getClass(), "/templates");
         cfg.setDefaultEncoding("UTF-8");
         cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
         return cfg;
