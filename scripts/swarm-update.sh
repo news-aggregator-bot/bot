3a9f60acf1df64b71c7fd0280a194d4fee9575a3
v.yemelianov <v.yemelianov@voverc.com>
2020-08-20 10:56:59 +0700

    debug update

--- scripts/swarm-update.sh
+++ scripts/swarm-update.sh
@@ -1,4 +1,5 @@
 wget -q $1 -O docker-stack-bot.yml
+env
 echo "Deploying new na-bot stack"
 docker stack rm na-bot
 docker stack deploy --compose-file docker-stack-bot.yml --with-registry-auth na-bot
