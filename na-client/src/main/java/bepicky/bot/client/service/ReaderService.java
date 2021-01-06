5f77c415821a1946c395f19cde55ba777841edb7
v.yemelianov <v.yemelianov@voverc.com>
2021-01-06 13:05:35 +0700

    empty primary language registration fix

--- na-client/src/main/java/bepicky/bot/client/service/ReaderService.java
+++ na-client/src/main/java/bepicky/bot/client/service/ReaderService.java
@@ -5,12 +5,14 @@ import bepicky.common.domain.dto.ReaderDto;
 import bepicky.common.domain.dto.StatusReaderDto;
 import bepicky.common.domain.request.ReaderRequest;
 import bepicky.common.exception.ResourceNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class ReaderService implements IReaderService {
 
     @Autowired
@@ -18,6 +20,7 @@ public class ReaderService implements IReaderService {
 
     @Override
     public ReaderDto register(ReaderRequest readerRequest) {
+        log.info("reader:registration:{}", readerRequest.toString());
         return readerClient.register(readerRequest);
     }
 
